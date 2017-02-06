ad31c5a14d4eaa00c733f6d645986c005607ef0b
Austin <acheese1234@gmail.com>
2017-02-06 16:53:30 -0500

    Added the GameManager class and header files.

---
+++ SETTrek/SETTrek/GameManager.cpp
@@ -0,0 +1,21 @@
+/*
+* PROJECT		: SETTrek
+* FILE			: GameManager.cpp
+* PROGRAMMER	: Austin Che
+* DATE			: 2017/02/06
+* DESCRIPTION	: The CPP file implementation of the GameManager class.
+*/
+#include "GameManager.h"
+
+
+
+/**
+* \brief
+* \details
+* \param
+* \return
+*/
+bool GameManager::InitGame(void)
+{
+	return true;
+}
---
+++ SETTrek/SETTrek/GameManager.h
@@ -0,0 +1,27 @@
+/*
+* PROJECT		: SETTrek
+* FILE			: GameManager.h
+* PROGRAMMER	: Austin Che
+* DATE			: 2017/02/06
+* DESCRIPTION	: The GameManager is our specialized entry point.
+*			Think if the GameManager class as the specialized 'Main'
+*			that will be the top-parent of the game objects, inputs,
+*			outpus, updating and rendering.
+*/
+#pragma once
+
+
+/*
+* \class GameManager
+* \brief
+*/
+class GameManager
+{
+private:
+
+
+
+public:
+
+	bool InitGame(void);
+};

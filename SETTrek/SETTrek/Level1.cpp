1be82e703bbf4043695bd55eb145ce9b32dea7fe
Austin <acheese1234@gmail.com>
2017-02-14 17:41:04 -0500

    Implemented some game logic - rendering the ship, background, planets. Made the ship move across the screen (in x direction).
    TODO: Create a GameObject instead of using the SpriteSheet. As well, GameObject should have coordinate attributes, instead of level having the (x,y) coordinates of Sprites.

--- SETTrek/SETTrek/Level1.cpp
+++ SETTrek/SETTrek/Level1.cpp
@@ -17,7 +17,31 @@
 */
 void Level1::Load(void)
 {
+	// Ensure the random number generator is operation
+	srand((unsigned int)time(NULL));
 
+	// TEMP
+	windowWidth = 1008.0f; // Original: 1024
+	windowHeight = 729.0f; // Original: 768
+
+	gridWidth = windowWidth / kNumberOfGrid;
+	gridHeight = windowHeight / kNumberOfGrid;
+
+	shipX = 0.0f;
+
+	// Loading the assets, resources, images, etc.
+	pBackground = new SpriteSheet(L".\\assets\\SectorBackground.bmp", gfx);
+	pStarShip = new SpriteSheet(L".\\assets\\ShipBase.bmp", gfx);
+	pPlanet1 = new SpriteSheet(L".\\assets\\Planet1.bmp", gfx);
+	pPlanet2 = new SpriteSheet(L".\\assets\\Planet2.bmp", gfx);
+	pPlanet3 = new SpriteSheet(L".\\assets\\Planet3.bmp", gfx);
+	numberOfGeneratedPlanets = 0;
+
+
+	// Generate the grid positions/coordinates
+	GenerateGrid();
+	GenerateRandomGrid();
+	GenerateRandomPlanet();
 }
 
 
@@ -29,7 +53,12 @@ void Level1::Load(void)
 */
 void Level1::Unload(void)
 {
-
+	// Clean up any resources
+	if (pBackground) delete pBackground;
+	if (pStarShip) delete pStarShip;
+	if (pPlanet1) delete pPlanet1;
+	if (pPlanet2) delete pPlanet2;
+	if (pPlanet3) delete pPlanet3;
 }
 
 
@@ -41,7 +70,20 @@ void Level1::Unload(void)
 */
 void Level1::Update(void)
 {
+	shipX = shipX + gridWidth;
 
+	// The ship has left the screen
+	if (shipX > windowWidth)
+	{
+		// Reset the ship's position to the beginning
+		shipX = 0;
+
+		// Generate another random set of coordinates for the planets
+		chosenGrid.clear();
+		GenerateRandomGrid();
+		chosenPlanets.clear();
+		GenerateRandomPlanet();
+	}
 }
 
 
@@ -53,5 +95,119 @@ void Level1::Update(void)
 */
 void Level1::Render(void)
 {
+	pBackground->Draw(0, 0, windowWidth, windowHeight);
+	pStarShip->Draw(shipX, spaceGrid[kMiddleGrid - 1].first, gridWidth + shipX, spaceGrid[kMiddleGrid - 1].first + gridHeight);
 
+
+	for (int i = 0; i < chosenGrid.size(); ++i)
+	{
+		chosenPlanets[i]->Draw(
+			chosenGrid[i].first,
+			chosenGrid[i].second,
+			chosenGrid[i].first + gridWidth,
+			chosenGrid[i].second + gridHeight
+		);
+	}
+
+	//for (int i = 0; i < spaceGrid.size(); ++i)
+	//{
+	//	pStarShip->Draw(
+	//		spaceGrid[i].first,
+	//		spaceGrid[i].second,
+	//		spaceGrid[i].first + gridWidth,
+	//		spaceGrid[i].second + gridHeight
+	//		);
+	//}
+}
+
+
+/**
+* \brief
+* \details
+* \param
+* \return
+*/
+void Level1::GenerateGrid(void)
+{
+	int row = 0;		// X-axis
+	int col = 0;		// Y-axis
+
+	for (col = 0; col < kNumberOfGrid; ++col)
+	{
+		for (int row = 0; row < kNumberOfGrid; ++row)
+		{
+			// <x, y>
+			pair<float, float> coord;
+			coord.first = row * gridWidth;
+			coord.second = col * gridHeight;
+
+			spaceGrid.push_back(coord);
+		}
+	}
+}
+
+
+/**
+* \brief
+* \details
+* \param
+* \return
+*/
+void Level1::GenerateRandomGrid(void)
+{
+	bool isSpawn = false;
+	int chance = 0;
+	
+	// For each spaceGrid position, calculate the chance
+	// for a planet to spawn. There is a 1 in 20 (5%) chance
+	// that a planet will spawn...
+	for (int i = 0; i < spaceGrid.size(); ++i)
+	{
+		isSpawn = (rand() % 100) < 5;
+
+		if (isSpawn)
+		{
+			// These are the chose grid positions where
+			// a planet is going to be drawn at
+			pair<float, float> coord;
+			coord.first = spaceGrid[i].first;
+			coord.second = spaceGrid[i].second;
+			chosenGrid.push_back(coord);
+
+			++numberOfGeneratedPlanets;
+			isSpawn = false;
+		}
+	}
+}
+
+
+/**
+* \brief
+* \details
+* \param
+* \return
+*/
+void Level1::GenerateRandomPlanet(void)
+{
+	int chosenPlanet = 0;
+
+	// For each chosen grid coordinate - we choose
+	// what planets to spawn at that coordinate. It
+	// could be Earth, Mars, or Jupiter
+	for (size_t i = 0; i < chosenGrid.size(); ++i)
+	{
+		chosenPlanet = rand() % 3;
+		if (chosenPlanet == 0)
+		{
+			chosenPlanets.push_back(pPlanet1);
+		}
+		else if (chosenPlanet == 1)
+		{
+			chosenPlanets.push_back(pPlanet2);
+		}
+		else if (chosenPlanet == 2)
+		{
+			chosenPlanets.push_back(pPlanet3);
+		}
+	}
 }
--- SETTrek/SETTrek/Level1.h
+++ SETTrek/SETTrek/Level1.h
@@ -7,7 +7,14 @@
 */
 #pragma once
 #include "Level.h"
+#include <time.h>
+#include <vector>
+using namespace std;
 
+
+//-CONSTANTS
+#define kNumberOfGrid		10
+#define kMiddleGrid			4
 
 
 
@@ -21,7 +28,27 @@ class Level1 : public Level
 {
 private:
 
+	RECT screenSize;				//!< The size of the Window (the game area)
+	SpriteSheet* pBackground;		//!< The background sprite for the game
+	SpriteSheet* pStarShip;			//!< The starship
+	SpriteSheet* pPlanet1;			//!< The first planet
+	SpriteSheet* pPlanet2;			//!< The second planet
+	SpriteSheet* pPlanet3;			//!< The third planet
 
+	float shipX;					//!< For moving the ship across the screen
+
+	float windowWidth;				//!< The total width of the window (x-direction)
+	float windowHeight;				//!< The total height of the window (y-direction)
+
+	float gridWidth;				//!< The width of 1 grid space
+	float gridHeight;				//!< The height of 1 grid space
+
+
+	int numberOfGeneratedPlanets;
+
+	vector<pair<float, float>> spaceGrid;		//!< Contains the absolute position for the 10 by 10 grid
+	vector<pair<float, float>> chosenGrid;		//!< Contains the grid coordinates of where a random planet may spawn
+	vector<SpriteSheet *> chosenPlanets;			//!< Contains the planets (a random chance) that will be spawned
 
 public:
 
@@ -29,4 +56,8 @@ public:
 	void Unload(void) override;		// For unloading assets from the game level
 	void Update(void) override;		// For updating the game level
 	void Render(void) override;		// For rendering the game level
+
+	void GenerateGrid(void);
+	void GenerateRandomGrid(void);
+	void GenerateRandomPlanet(void);
 };

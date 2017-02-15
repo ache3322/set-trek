6902aeb2308ec4d1dd553464adb8f2cb67cecdb2
Austin <acheese1234@gmail.com>
2017-02-15 17:14:33 -0500

    MAJOR overhaul of the Graphics class. Now using the Direct2D 1.1 (for Windows 8.1 and later).
    TODO: Set up D2D1Effects (or create class)

--- SETTrek/SETTrek/Graphics.cpp
+++ SETTrek/SETTrek/Graphics.cpp
@@ -15,9 +15,21 @@
 */
 Graphics::Graphics()
 {
-	factory = NULL;
-	rendertarget = NULL;
 	brush = NULL;
+
+	d3d11Device = NULL;
+	d3d11Context = NULL;
+
+	d2d1Factory1 = NULL;
+	d2d1Context = NULL;
+	d2d1Device = NULL;
+	dxgiSwapChain = NULL;
+	d2d1Bitmap = NULL;
+
+	parameters.DirtyRectsCount = 0;
+	parameters.pDirtyRects = nullptr;
+	parameters.pScrollRect = nullptr;
+	parameters.pScrollOffset = nullptr;
 }
 
 /**
@@ -27,16 +39,21 @@ Graphics::Graphics()
 */
 Graphics::~Graphics()
 {
-	if (factory) factory->Release();
-	if (rendertarget) rendertarget->Release();
-	if (brush) brush->Release();
+	if (d3d11Device) d3d11Device->Release();
+	if (d3d11Context) d3d11Context->Release();
+	if (d2d1Factory1) d2d1Factory1->Release();
+	if (d2d1Context) d2d1Context->Release();
+	if (d2d1Device) d2d1Device->Release();
+	if (dxgiSwapChain) dxgiSwapChain->Release();
+	if (d2d1Bitmap) d2d1Bitmap->Release();
 }
 
 
 /**
 * \brief Initialize the Graphics object and all COM components.
 * \details Initialize the necessary resources to use the Direct2D graphics.
-*		It includes binding the client's window to the COM.
+*		It includes binding the client's window to the COM.<br/>
+* Credit: https://katyscode.wordpress.com/2013/01/23/migrating-existing-direct2d-applications-to-use-direct2d-1-1-functionality-in-windows-7/
 * \param windowHandle - HWND - A handle to the client's 
 * \return bool : If Graphics is able to initialize.
 */
@@ -44,43 +61,196 @@ bool Graphics::Init(HWND windowHandle)
 {
 	CoInitialize(nullptr);
 
-	// Create D2D1 object to be used to create D2D1 resources.
-	HRESULT res = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &factory);
-	if (res != S_OK) return false;
+	//=====================
+	//-------------------
+	//--- VARIABLE DECLARATION
 
+	// Creating Direct3D device and context - enables
+	// us to use functionalities to be supported in Direct2D
+	ComPtr<ID3D11Device> device;
+	ComPtr<ID3D11DeviceContext> context;
+	ComPtr<IDXGIDevice> dxgiDevice;
+	
+	//=======================
+	//----------------------
+
+	D2D1_FACTORY_OPTIONS options;
+	ZeroMemory(&options, sizeof(D2D1_FACTORY_OPTIONS));
+
+	// Create D2D1 object to be used to create D2D1 resources
+	HRESULT hr = D2D1CreateFactory(
+		D2D1_FACTORY_TYPE_SINGLE_THREADED,
+		__uuidof(ID2D1Factory1),
+		&options,
+		reinterpret_cast<void **>(&d2d1Factory1));
+	if (hr != S_OK) return false;
+
+	// Acquire the client window size - for rendering targets on this dimension
 	RECT rect;
-	GetClientRect(windowHandle, &rect); // Set the rect's right and bottom properties = the client window's size
+	GetClientRect(windowHandle, &rect);
+	D2D1_SIZE_U clientSize = D2D1::SizeU(rect.right, rect.bottom);
 
 	// Create a render target to render window
-	// HwndRenderTargetProperties:
-	//    HWND - target issues output from its drawing commands
-	//    pixelSize - sizeo of the redner target, in pixels
-	//		> We use the rectangle size of the client window
-	//    D2D1_PRESENT_OPTIONS - specifies whether target retains frame after it is presented
-	//		> rendertarget is the render targe
-	res = factory->CreateHwndRenderTarget(
+	hr = d2d1Factory1->CreateHwndRenderTarget(
 		D2D1::RenderTargetProperties(),
-		D2D1::HwndRenderTargetProperties(windowHandle, D2D1::SizeU(rect.right, rect.bottom)),
-		&rendertarget);
-	if (res != S_OK) {
-		return false;
-	}
+		D2D1::HwndRenderTargetProperties(windowHandle, clientSize),
+		(ID2D1HwndRenderTarget **)&d2d1Context);
+	if (hr != S_OK) return false;
+
+	// Set feature levels supported by our application
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+		D3D_FEATURE_LEVEL_9_3,
+		D3D_FEATURE_LEVEL_9_2,
+		D3D_FEATURE_LEVEL_9_1
+	};
 	
-	res = rendertarget->CreateSolidColorBrush(D2D1::ColorF(0, 0, 0, 0), &brush);
-	if (res != S_OK) {
-		return false;
-	}
+	// Adding this flag supports different color channels
+	UINT creationFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
+
+	// Creating the Direct32 device
+	hr = D3D11CreateDevice(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		0,
+		creationFlags,
+		featureLevels,
+		ARRAYSIZE(featureLevels),
+		D3D11_SDK_VERSION,
+		&device,
+		NULL,
+		&context
+	);
+	if (hr != S_OK) return false;
+
+	// Fetching underlying interfaces for the D3D11
+	hr = device.Get()->QueryInterface(__uuidof(ID3D11Device1), (void **)&d3d11Device);
+	hr = context.Get()->QueryInterface(__uuidof(ID3D11DeviceContext1), (void **)&d3d11Context);
+	if (hr != S_OK) return false;
+
+	// Fetching the underlying DXGI device
+	hr = d3d11Device->QueryInterface(__uuidof(IDXGIDevice), (void **)&dxgiDevice);
+	if (hr != S_OK) return false;
+
+	// Now create the Direct2D context
+	hr = d2d1Factory1->CreateDevice(dxgiDevice.Get(), &d2d1Device);
+	if (hr != S_OK) return false;
+
+	// Get the corresponding device context object
+	hr = d2d1Device->CreateDeviceContext(
+		D2D1_DEVICE_CONTEXT_OPTIONS_NONE,
+		&d2d1Context);
+	if (hr != S_OK) return false;
+
+
+	//=====================
+	//-------------------
+	//--- SWAP CHAINING
+
+	// Variables for Swap Chaining
+	ComPtr<IDXGIAdapter> dxgiAdapter;
+	ComPtr<IDXGIFactory2> dxgiFactory;
+	FLOAT dpiX = 0.f;
+	FLOAT dpiY = 0.f;
+
+	// 1. Getting the DXGI Factory object and adapter
+	dxgiDevice->GetAdapter(&dxgiAdapter);
+	if (hr != S_OK) return false;
+
+	// 2. Get an instance of the DXGI factory
+	dxgiAdapter->GetParent(IID_PPV_ARGS(&dxgiFactory));
+	if (hr != S_OK) return false;
+
+	// 3. Defining and describing the swap chain
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = { 0 };
+	swapChainDesc.Width = 0;        // use automatic sizing
+	swapChainDesc.Height = 0;		// determined by client size
+	swapChainDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+	swapChainDesc.Stereo = false;
+	swapChainDesc.SampleDesc.Count = 1;                // don't use multi-sampling
+	swapChainDesc.SampleDesc.Quality = 0;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 2;                     // use double buffering to enable flip
+	swapChainDesc.Scaling = DXGI_SCALING_STRETCH;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD; // Disabling the swap effects
+	swapChainDesc.Flags = 0;
+
+	// 4. Create the Swap Chain for the Window Handle
+	hr = dxgiFactory->CreateSwapChainForHwnd(
+		d3d11Device,
+		windowHandle,
+		&swapChainDesc,
+		nullptr,
+		nullptr,
+		&dxgiSwapChain
+	);
+	if (hr != S_OK) return false;
+
+	// 5. Get the backbuffer
+	ComPtr<IDXGISurface> dxgiBackBuffer;
+	dxgiSwapChain->GetBuffer(0, IID_PPV_ARGS(&dxgiBackBuffer));
+	if (hr != S_OK) return false;
+
+	// 6. Creation of a bitmap needed to be the render target
+	d2d1Factory1->GetDesktopDpi(&dpiX, &dpiY);
+
+	D2D1_BITMAP_PROPERTIES1 bitmapProperties =
+		D2D1::BitmapProperties1(
+			D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW,
+			D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_IGNORE),
+			dpiX,
+			dpiY
+		);
+
+	d2d1Context->CreateBitmapFromDxgiSurface(
+		dxgiBackBuffer.Get(),
+		bitmapProperties,
+		&d2d1Bitmap);
+	if (hr != S_OK) return false;
+
+	// 7. Finally! Now set the bitmap as the Device Context's render target
+	d2d1Context->SetTarget(d2d1Bitmap);
+	//---
+	//-------------------
+	//=====================
+
+	// Create a solid brush from the DeviceContext
+	hr = d2d1Context->CreateSolidColorBrush(D2D1::ColorF(0, 0, 0, 0), &brush);
+	if (hr != S_OK) return false;
+
 	return true;
 }
 
 
 /**
-* \brief Get the render target.
-* \return ID2D1RenderTarget* - A reference to the render target.
+* \brief End drawing the graphics
+* \param void
+* \return void
 */
-ID2D1RenderTarget* Graphics::GetRenderTarget(void)
+void Graphics::EndDraw(void) 
+{ 
+	d2d1Context->EndDraw();
+
+	// Long time ago, in Direct2D the render target
+	// was automatically swapped chained. However, with
+	// the new Direct2D we must manually do it
+	dxgiSwapChain->Present1(1, 0, &parameters);
+}
+
+
+/**
+* \brief Get the device context target.
+* \details The DeviceContext expands upon the RenderTarget. It provides
+*	the same methods, but even more. You can use the DeviceContext for effects.
+* \return ID2D1DeviceContext* - A reference to the device context target.
+*/
+ID2D1DeviceContext* Graphics::GetDeviceContext(void)
 {
-	return rendertarget;
+	return d2d1Context;
 }
 
 
@@ -94,7 +264,7 @@ ID2D1RenderTarget* Graphics::GetRenderTarget(void)
 void Graphics::ClearScreen(float r, float g, float b) 
 {
 	// Clear() takes in a D2D1_COLOR_F*
-	rendertarget->Clear(D2D1::ColorF(r, g, b));
+	d2d1Context->Clear(D2D1::ColorF(r, g, b));
 }
 
 
@@ -114,7 +284,7 @@ void Graphics::DrawCircle(float x, float y, float radius, float r, float g, floa
 	brush->SetColor(D2D1::ColorF(r, g, b, a));
 	// Render the shape on the target...
 	// Ellipse takes a position (x,y); width; height; brush; and brush stroke
-	rendertarget->DrawEllipse(D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius), brush, 3.0f);	
+	d2d1Context->DrawEllipse(D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius), brush, 3.0f);	
 
 }
 
@@ -124,5 +294,5 @@ void Graphics::DrawRectangle(float left, float top, float right, float bottom, f
 	brush->SetColor(D2D1::ColorF(r, g, b, a));
 
 	// Render the shape on the target...
-	rendertarget->DrawRectangle(D2D1::Rect(left, top, right, bottom), brush, 3.0f);
+	d2d1Context->DrawRectangle(D2D1::Rect(left, top, right, bottom), brush, 3.0f);
 }
--- SETTrek/SETTrek/Graphics.h
+++ SETTrek/SETTrek/Graphics.h
@@ -1,14 +1,20 @@
 /*
 * PROJECT		: SETTrek
 * FILE			: Graphics.h
-* PROGRAMMER	:
+* PROGRAMMER	: Austin Che
 * DATE			: 2017/01/25
 * DESCRIPTION	: The header file containing the class definition for the Graphics.
 */
 #pragma once
 #include <Windows.h>
-#include <d2d1.h>
 
+// Newly Direct2D for Windows 8.1 and later
+#include <wrl.h>
+#include <wrl/client.h>
+#include <d2d1_1.h>
+#include <d2d1_1helper.h>
+#include <d3d11_1.h>
+using namespace Microsoft::WRL;	// For using ComPtr
 
 
 /**
@@ -19,10 +25,22 @@ class Graphics
 {
 private:
 
-	ID2D1Factory* factory; //The factory allows us to create many other types of D2D resources
-	ID2D1HwndRenderTarget* rendertarget; //this is typically an area in our GPU memory.. like a back buffer 
+	// Previous things....
 	ID2D1SolidColorBrush* brush; //Note this COM interface! Remember to release it!
-	
+
+	// Direct3Dx11 objects
+	ID3D11Device1* d3d11Device;				//!<
+	ID3D11DeviceContext1* d3d11Context;		//!<
+
+	// New Direct2D 1.1
+	ID2D1Factory1* d2d1Factory1;			//!<
+	ID2D1DeviceContext* d2d1Context;		//!<
+	ID2D1Device* d2d1Device;				//!<
+	IDXGISwapChain1* dxgiSwapChain;			//!<
+	ID2D1Bitmap1* d2d1Bitmap;				//!< Direct 2D bitmap used as the render target for device context
+
+	DXGI_PRESENT_PARAMETERS parameters;
+
 public:
 	Graphics();
 
@@ -30,9 +48,9 @@ public:
 
 	bool Init(HWND windowHandle);
 
-	void BeginDraw() { rendertarget->BeginDraw(); }
-	void EndDraw() { rendertarget->EndDraw(); }
-	ID2D1RenderTarget* GetRenderTarget(void);
+	void BeginDraw(void) { d2d1Context->BeginDraw(); }
+	void EndDraw(void);
+	ID2D1DeviceContext* GetDeviceContext(void);
 
 	void ClearScreen(float r, float g, float b);
 	void DrawCircle(float c, float y, float radius, float r, float g, float b, float a);
